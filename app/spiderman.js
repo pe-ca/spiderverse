d95b81f8c47194bfeb9dcd4598be5c78c0020d48
pe-ca <mr.gloser1@gmail.com>
2022-05-01 14:34:54 -0500

    refactorizado el primer caso del test 5

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,9 +1,10 @@
 class Spiderman{
-    constructor(name, age, actor, movies){
+    constructor(name, age, actor, movies, studio){
         this.name = name
         this.age = age
         this.actor = actor
         this.movies = movies
+        this.studio = studio
     }
 
 }
