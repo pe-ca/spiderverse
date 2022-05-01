fa0c31c7496d6511b9bfb355bfa05db2e92c5b1a
pe-ca <mr.gloser1@gmail.com>
2022-05-01 14:13:23 -0500

    agregado dummy test

---
+++ test/spiderman.test.js
@@ -0,0 +1,6 @@
+describe("test suite dummy description", ()=>{
+    TextDecoderStream('case 1 dummy',()=>{
+        const resultOfSomething = 1 + 2
+        expect(resultOfSomething).toBe(18)
+    })
+})
