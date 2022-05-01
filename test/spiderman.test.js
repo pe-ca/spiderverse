e41060119855b8a84035b3d386eacb75ebe11336
pe-ca <mr.gloser1@gmail.com>
2022-05-01 14:20:46 -0500

    agregados casos de pruebas

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,11 @@
 describe("test suite dummy description", ()=>{
     TextDecoderStream('case 1 dummy',()=>{
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18)
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
     })
 })
